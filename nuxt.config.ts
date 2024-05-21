6d783da9999b791676614f4128929a7e620cf39d
iampiku <iampikuchatterjee@gmail.com>
2024-05-21 15:34:59 +0530

    🛠️ meta config added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  css: ['~/css/tailwind.css'],
-  modules: ['nuxt-swiper'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-})
+	app: {
+		head: {
+			meta: [
+				{
+					name: "google-site-verification",
+					content: "zsKC6Tr_t1ube1CFXA06gMkQuYI5n3N9VWAsmfNlX98",
+				},
+			],
+		},
+	},
+	devtools: { enabled: false },
+	css: ["~/css/tailwind.css"],
+	modules: ["nuxt-swiper"],
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
+});
