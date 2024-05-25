e9f4d569679486bb1755adac4bf98f6c2979f57a
Pradipta Chatterjee <iampikuchatterjee@gmail.com>
2024-05-25 22:46:39 +0530

    site meta config updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,24 @@
 export default defineNuxtConfig({
 	app: {
 		head: {
+			charset: "utf-8",
+			viewport: "width=device-width, initial-scale=1",
+			script: [
+				{
+					async: true,
+					src: "https://www.googletagmanager.com/gtag/js?id=G-4C7L5SRT85",
+				},
+				{
+					hid: "gtag-inline",
+					innerHTML: `
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-4C7L5SRT85');
+					`,
+					type: "text/javascript",
+				},
+			],
 			meta: [
 				{
 					name: "google-site-verification",
